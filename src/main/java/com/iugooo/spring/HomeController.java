24fd5ce2eb432a9a73d34fa4d209ea10c532ba61
David Liu <mingchang716@gmail.com>
2014-12-09 00:31:01 -0600

    add loginPage and login method

--- src/main/java/com/iugooo/spring/HomeController.java
+++ src/main/java/com/iugooo/spring/HomeController.java
@@ -8,11 +8,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Handles requests for the application home page.
+ * 
+ * controller annotation is used to indicate that it is a web controller class.
  */
 @Controller
 public class HomeController {
@@ -21,6 +24,9 @@ public class HomeController {
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
+	 * 
+	 * RequestMapping is used with classes and methods to redirect client request to specific handler method.
+	 * Notice that handler methods are returning String, this should be the name of view page to be used as response.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
@@ -35,5 +41,19 @@ public class HomeController {
 		
 		return "home";
 	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String loginPage(Locale locale, Model model){
+		return "login";
+	}
+	
+	@RequestMapping(value = "/home", method = RequestMethod.POST)
+	public String login(@Validated User user, Model model){
+		model.addAttribute("userName", user.getUserName());
+		
+		return "user";
+	}
+	
+	
 	
 }
